625284c8325a404e4c830f8636f2adabb83f7a71
nigmatini <ntinioedwards@students.utech.edu.jm>
2023-03-28 23:24:22 -0500

    Student and Staff Dashboard added
    Placeholder classes for dashboards

---
+++ element_test/StudentDashboard.java
@@ -0,0 +1,13 @@
+package com.element_test;
+
+import javax.swing.JOptionPane;
+
+public class StudentDashboard {
+
+	public static void main(String[] args) {
+		new StudentDashboard();
+		JOptionPane.showMessageDialog(null, "This is the dashboard placeholder", "Dashboard", JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
